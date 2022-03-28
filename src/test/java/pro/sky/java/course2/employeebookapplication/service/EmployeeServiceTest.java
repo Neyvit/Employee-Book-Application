996b522cf58bd88e48cad35b6d0f55194fdf6ac3
Komlev Vitaly <successfullproject@gmail.com>
2022-03-29 01:23:35 +0300

    Junit test

---
+++ src/test/java/pro/sky/java/course2/employeebookapplication/service/EmployeeServiceTest.java
@@ -0,0 +1,24 @@
+package pro.sky.java.course2.employeebookapplication.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceTest {
+
+    @Test
+    void addEmployee() {
+    }
+
+    @Test
+    void removeEmployee() {
+    }
+
+    @Test
+    void findEmployee() {
+    }
+
+    @Test
+    void getAllEmployees() {
+    }
+}
